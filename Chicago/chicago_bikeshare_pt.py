a2b5c8c31ecad8e8bcc32c1426b959f54e584658
Arilson do Espirito Santo Brito <arilson_brito@yahoo.com.br>
2018-10-11 11:08:21 -0300

    Tarefa 1

--- Chicago/chicago_bikeshare_pt.py
+++ Chicago/chicago_bikeshare_pt.py
@@ -28,6 +28,8 @@ input("Aperte Enter para continuar...")
 # TAREFA 1
 # TODO: Imprima as primeiras 20 linhas usando um loop para identificar os dados.
 print("\n\nTAREFA 1: Imprimindo as primeiras 20 amostras")
+for i, data in enumerate(data_list[1:21]):
+    print("Linha {}. Dados: {}".format(i + 1, data))
 
 # Vamos mudar o data_list para remover o cabeçalho dele.
 data_list = data_list[1:]
